2ee4795da522c48d2912adb5452d62ae0ae8b85d
amolinarius <89464182+amolinarius@users.noreply.github.com>
2024-04-25 12:15:26 +0200

    Added Markdown slides parser

---
+++ src/script.js
@@ -0,0 +1,54 @@
+/**
+ * @param {string|string[]|{title?: string, pages: string|string[]}} _content
+ * @returns {{title: string, html: string}}
+ */
+function parseContent(_content) {
+    var result;
+    const converter = new showdown.Converter();
+    if (typeof _content === 'string') {
+        const html = _content.trim() !== '' ? converter.makeHtml(_content) : converter.makeHtml('# Empty Document');
+        result = {title: 'Untitled', html: `<div class="slide slide_0">${html}</div>`}
+    }
+    else if (isStringArray(_content)) {
+        result = {
+            title: 'Untitled',
+            html: _content.map((v, i)=>{
+                const html = v.trim() !== '' ? converter.makeHtml(v) : converter.makeHtml('# Empty Slide');
+                return `<div class="slide slide_${i}">${html}</div>`
+            }).join('')
+        }
+    }
+    else if (_content instanceof Object){
+        result = {
+            title: _content.title || 'Untitled',
+            html: _content.pages.trim() !== '' ? parseContent(_content.pages) : parseContent('# Empty Document')
+        }
+    }
+    return result;
+}
+function isStringArray(_array) {
+    if (!_array instanceof Array) {return false}
+    else {
+        var onlyStrings = true;
+        _array.forEach(v=>typeof v === 'string' ? '' : onlyStrings = false)
+        return onlyStrings
+    }
+}
+
+
+const hash = window.location.hash;
+var content;
+if (hash === '') {content = ''}
+else {
+    try {
+        const json = atob(hash.slice(1));
+        console.log(json);
+        try {content = JSON.parse(json)}
+        catch {content = '# Invalid hash value : Unable to parse JSON'}
+    }
+    catch {content = '# Invalid hash value. Please make sure it\'s encoded in base64.'}
+}
+const parsedContent = parseContent(content);
+document.querySelector('.slides-container').innerHTML = parsedContent.html;
+document.querySelector('title').textContent = parsedContent.title;
+console.log(btoa('"# Heading 1 \\n## Heading 2\\n### Heading 3\\n> ### Note\\nI\'m still in blockquote\\n\\nParagraph with **bold text** and _italic_ one"'));
